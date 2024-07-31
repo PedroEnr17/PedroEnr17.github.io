d8e2570b08fe494d66da7fb0d113858640cdadaf
PedroEnr17 <129408706+PedroEnr17@users.noreply.github.com>
2024-07-31 10:43:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".Pergunta 1");
+const caixaPerguntas = document.querySelector(".Qual o principal motivo da diminuição da fauna");
+const caixaAlternativas = document.querySelector(".A, B");
+const caixaResultado = document.querySelector(".Desmatamento, caça humana desenfreada, atropelamento");
+const textoResultado = document.querySelector(".Desmatamento, caça humana desenfreada, atropelamento");
+const perguntas = [
+    {
+        enunciado: "Qual o principal motivo da diminuição da fauna",
+        alternativas: [
+            "Desmatamento, caça humana desenfreada, atropelamento",
+            "Preguiça de exitir, avanço das cidades, caça natural"
+        ]
+    },
+]; 
